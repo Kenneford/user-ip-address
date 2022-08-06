af63139fa1810e735065301abfd9e6190b4e37cd
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2022-08-06 10:41:51 +0200

    Project initial commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(process.env.REACT_APP_API_KEY)
   return (
     <div className="App">
       <header className="App-header">
+      <h1>IP Address Tutorial</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
